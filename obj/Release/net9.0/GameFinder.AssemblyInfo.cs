3d8ca2a23264eb856c606cda26208a4195a461d8
Chait <chaitknight81@gmail.com>
2025-06-02 00:46:33 -0400

    Save local changes before pulling

--- obj/Release/net9.0/GameFinder.AssemblyInfo.cs
+++ obj/Release/net9.0/GameFinder.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameFinder")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2abf06f2bbdd6385ab791382d9a63e50653835e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameFinder")]
